a58eaa69e4112f012a6b1959c7faca4874eb8df4
Srivathsan V <107630327+srivathsan-snowflake@users.noreply.github.com>
2022-06-16 19:42:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 #created the main python file
 import streamlit
 streamlit.title("My parents' new healthy diner")
-streamlit.caption("Menu")
+streamlit.header("Menu")
 streamlit.text("1")
 streamlit.text("2")
